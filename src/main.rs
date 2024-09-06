b2729f972599fc2ef2ac6273a78f17a5c082d825
Victor <vw1262@gmail.com>
2024-09-05 23:15:08 -0500

    quick and dirty implementation of first and second half avgs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use std::{error::Error, io, process};
+
+fn simple() -> Result<(), Box<dyn Error>> {
+    let mut reader = csv::ReaderBuilder::new()
+        .delimiter(b';')
+        .from_path("garmin-converted/tracks.csv")
+        .unwrap();
+
+    let mut count = 0;
+    let mut heart_rates = vec![];
+    for result in reader.records() {
+        let record = result?;
+        let heart_rate = record.get(7).unwrap().parse::<u32>().unwrap();
+
+        heart_rates.push(heart_rate);
+        // println!("{:?}", heart_rate);
+        count += 1;
+    }
+
+    // println!("{:?}", count);
+    let half_index = heart_rates.len() / 2;
+    let (first_half, second_half) = heart_rates.split_at(half_index);
+
+    let mut sum = 0;
+    for i in first_half {
+        sum += i;
+    }
+    let first_half_avg: f32 = sum as f32 / first_half.len() as f32;
+
+    // println!("{:?}", first_half);
+    let mut second_sum = 0;
+    for j in second_half {
+        second_sum += j;
+    }
+    let second_half_avg: f32 = second_sum as f32 / second_half.len() as f32;
+
+    println!("First: {}, Second {}", first_half_avg, second_half_avg);
+
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    if let Err(err) = simple() {
+        println!("error running example: {}", err);
+    }
 }
